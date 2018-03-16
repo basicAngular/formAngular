a0770a63599bb5da131908714885ef72154d7a3c
wdyogesh <wdyogesh@gmail.com>
2018-03-16 18:32:08 +0530

    add new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  myGroup: FormGroup;
+  
+
+  ngOnInit(){
+      this.myGroup = new FormGroup({
+      itemName: new FormControl(),
+      price: new FormControl(),
+      language: new FormControl()
+    });
+  }
+
   title = 'app';
+  langs: string[] = ['English', 'French', 'German']
 }
